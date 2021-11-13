56a831e87595b1dbb0fc8e2e80c4d5adedb97d15
Paritosh-J <paritoshj2001@gmail.com>
2021-11-13 18:54:26 +0530

    made changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from flask import Flask , render_template  , request 
 from tensorflow.keras.preprocessing.image import load_img 
@@ -8,8 +9,7 @@ from util import check
 app = Flask(__name__,template_folder='templates')
 BASE_PATH = os.getcwd()
 UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
-model = load_model(os.path.join(BASE_PATH , 'VGG_model.h5'))
-
+model = load_model(os.path.join(BASE_PATH , 'model_f.h5'))
 
 def names(number):
     if number==0:
@@ -17,33 +17,33 @@ def names(number):
     else:
         return 'No, Its not a tumor'
 
-
 def predict(filename , model):
-    res = check(filename)
+
+    # res = check(filename)
+    img = image.load_img(filename , target_size = (128, 128))
+    img = np.asarray(img)
+    img = np.expand_dims(img, axis=0)
+    res = model.predict(img)
     classification = np.where(res == np.amax(res))[1][0]
-    txt = str(res[0][classification]*100) + '% Confidence This Is ' + names(classification)
-    return  txt
-
-
-
+    txt = str(res[0][classification]*100) + ' % ' + names(classification)
+    return txt
 
 @app.route('/')
 def home():
         return render_template("index.html")
 
 @app.route('/success' , methods = ['GET' , 'POST'])
-def success():    
+def success():
     if request.method == 'POST':
-            upload_file = request.files['file']              
+            upload_file = request.files['file']
             filename = upload_file.filename
             path_save = os.path.join(UPLOAD_PATH,filename)
             upload_file.save(path_save)
-
-            text = predict(path_save , model)
+            text = predict(path_save, model)
+            # text = "helo"
 
             return render_template('success.html',img=filename,text_h=text)
     return render_template('index.html',upload=False)
 
 if __name__ == "__main__":
     app.run(debug = True)
-
--- util.py
+++ util.py
@@ -1,14 +1,18 @@
 import numpy as np
 from keras.preprocessing import image
 from tensorflow.keras.models import load_model
-saved_model = load_model("VGG_model.h5")
 
+saved_model = load_model("model_f.h5")
+
+# def names(number):
+#     if number==0:
+#         return 'Its a Tumor'
+#     else:
+#         return 'No, Its not a tumor'
 
 def check(input_img):
-   
-
-    img = image.load_img(input_img , target_size = (224 , 224))
+    img = image.load_img(input_img , target_size = (128, 128))
     img = np.asarray(img)
     img = np.expand_dims(img, axis=0)
-    output = saved_model.predict(img)    
+    output = saved_model.predict(img)
     return output
